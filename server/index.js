0e1fd5491241a27c910c03e466c335da2cd16090
Praveen R <praveen.ad22@bitsathy.ac.in>
2025-05-28 09:31:39 +0530

    build

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const passportconfig=require('./middleware/passport')
 const cors=require('cors')
 const authRoutes=require('./Routes/authRoutes')
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
 app.use(passport.initialize())
 app.use('/auth',authRoutes)
 app.use('/tasks',verifytoken,require('./Routes/TaskRoute'))
