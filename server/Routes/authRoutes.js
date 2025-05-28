fd2e65679c0970980bce000b8cc0a9d8a6c87eec
Praveen R <praveen.ad22@bitsathy.ac.in>
2025-05-28 12:01:35 +0530

    modified

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -10,20 +10,11 @@ router.get(
   })
 )
 
-router.get('/google/redirect', (req, res, next) => {
-  passport.authenticate('google', { session: false }, (err, user, info) => {
-    if (err) {
-      console.error('Passport authentication error:', err)
-      return res.status(500).json({ error: 'Authentication error', details: err.message })
-    }
-    if (!user) {
-      console.warn('No user returned from Google OAuth:', info)
-      return res.status(401).json({ error: 'User not authenticated' })
-    }
-
-    req.user = user
-    next()
-  })(req, res, next)
-}, googleAuth)
-
+router.get(
+  '/google/redirect',
+  passport.authenticate('google', {
+    session: false,
+  }),
+  googleAuth
+)
 module.exports = router
