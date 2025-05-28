0c6f9d936962f75a9b88ba5c428e85d1a1315201
Praveen R <praveen.ad22@bitsathy.ac.in>
2025-05-28 11:27:23 +0530

    modified

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,30 +1,36 @@
 const passport = require('passport')
-const googleStrategy = require('passport-google-oauth20').Strategy
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const User = require('../model/User')
 
 passport.use(
-  new googleStrategy(
+  new GoogleStrategy(
     {
       callbackURL:
         'https://to-do-app-api-swart.vercel.app/auth/google/redirect',
-      clientID: process.env.clientID,
-      clientSecret: process.env.clientSecret,
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
     },
-    async (acessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       try {
+        if (!profile.emails || !profile.photos) {
+          return done(new Error('Missing email or photo from Google'), null)
+        }
+
         const user = await User.findOne({ email: profile.emails[0].value })
         if (user) {
           return done(null, user)
         }
-        const newuser = new User({
+
+        const newUser = new User({
           username: profile.displayName,
           email: profile.emails[0].value,
           profile: profile.photos[0].value,
         })
-        await newuser.save()
-        done(null, newuser)
+
+        await newUser.save()
+        done(null, newUser)
       } catch (error) {
+        console.error('Passport Strategy Error:', error)
         done(error, null)
       }
     }
